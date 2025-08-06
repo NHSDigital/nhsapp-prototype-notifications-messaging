7071cd74b8001187132cacded8095de97049a316
joshavery1 <105367456+joshavery1@users.noreply.github.com>
2025-08-06 16:07:57 +0100

    Trying out new routes option

--- app/routes.js
+++ app/routes.js
@@ -201,25 +201,5 @@ router.post('/pages-rnib/services/gp-appointment-invite-answer', function (req, 
   }
 })
 
-// Adding "query" to every page. There could be a better implementaion using middleware. 
-// in nunjkucks you can call {{query[key]}}
-
-router.get('*', function(req, res) {
-    let path = req.params[0]
-    let pathEnd = path.slice(-1)
-    let newPath = path.substring(1)
-    if (path == "/") {
-      // Top home page
-      path = "index"
-    } else if (pathEnd == "/") {
-      // Check if this is the route folder, if so, then render the index page.
-      path = newPath + "index"
-    } else {
-      path = newPath
-    }
-    res.render(path, {
-      "query": req.query,
-    });
-  })
 
 module.exports = router;
