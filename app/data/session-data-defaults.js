f6c6cad92e056b5ed9ff7c8a79a76dd79c2d673e
joshavery1 <105367456+joshavery1@users.noreply.github.com>
2025-08-12 14:24:47 +0100

    Updated default data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -23,9 +23,9 @@ module.exports = {
   "version": "2.36.6 (2.36.0)",
 
   // Messages
-  "message1read": "false", 
-  "message2read": "false", 
-  "messages": 2, // This displays in the messages badge - set this to the number of unread messages - For example, if 'message1read = true' and 'message2read = false', then set the 'messages' number to 1
+  "message1read": "true", 
+  "message2read": "true", 
+  "messages": 0, // This displays in the messages badge - set this to the number of unread messages - For example, if 'message1read = true' and 'message2read = false', then set the 'messages' number to 1
   "messageSendingError": "true", // Shows 'cannot send your reply' screen for message 2
 
   // Set account type - p9, p5 or linked-profiles
